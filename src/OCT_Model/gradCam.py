0d797a6b2b21ac62e4c06ab1da1275e42f9cf3a3
Gil Sanzovo <gil@sanzovo.com.br>
2021-08-18 16:41:11 -0400

    Added the gradCam.py file inside the src/OCT_Model folder

---
+++ src/OCT_Model/gradCam.py
@@ -0,0 +1,200 @@
+
+import matplotlib.font_manager as fm
+from matplotlib.collections import QuadMesh
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Model
+import tensorflow.keras.backend as K
+# Display
+from IPython.display import Image, display
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from tensorflow import keras
+
+##################################################
+#
+#              Grad CAM
+#
+##################################################
+
+# Version 1:
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    # then sum all the channels to obtain the heatmap class activation
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+
+def save_and_display_gradcam(image, heatmap, cam_path="cam.jpg", alpha=0.4, disp=False):
+    # Load the original image
+    img = image
+    img = keras.preprocessing.image.img_to_array(img)
+
+    # Rescale heatmap to a range 0-255
+    heatmap = np.uint8(255 * heatmap)
+
+    # Use jet colormap to colorize heatmap
+    jet = cm.get_cmap("jet")
+
+    # Use RGB values of the colormap
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+
+    # Create an image with RGB colorized heatmap
+    jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+
+    # Superimpose the heatmap on original image
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+
+    # Save the superimposed image
+    superimposed_img.save(cam_path)
+
+    # Display Grad CAM
+    if disp:
+        display(Image(cam_path))
+
+
+# Version 2:
+def gradCAM(generator,model,params,image_num = 0, intensity=0.5, res=250):
+    images,_ = next(generator)
+    # Path to original image
+    path_to_image = params['data']["test_path"] + "/"+generator.filenames[0]
+
+    x = images[0]
+    x=np.expand_dims(x, axis=0)
+    preds = model.predict(x)
+
+    with tf.GradientTape() as tape:
+        last_conv_layer = model.get_layer('post_relu')
+        iterate = tf.keras.models.Model([model.inputs], [model.output, last_conv_layer.output])
+        model_out, last_conv_layer = iterate(x)
+        class_out = model_out[:, np.argmax(model_out[0])]
+        grads = tape.gradient(class_out, last_conv_layer)
+        pooled_grads = K.mean(grads, axis=(0, 1, 2))
+
+    heatmap = tf.reduce_mean(tf.multiply(pooled_grads, last_conv_layer), axis=-1)
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap)
+    heatmap = heatmap.reshape((7,7))
+
+    orig = cv2.imread(path_to_image)
+    orig = cv2.cvtColor(orig, cv2.COLOR_BGR2RGB)
+
+
+    heatmap = cv2.resize(heatmap, (orig.shape[1], orig.shape[0]))
+    heatmap = cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET)
+
+    img = heatmap * intensity + orig
+    show_image_list([orig.astype('uint8'),img.astype('uint8')],list_titles=["Original","gradCam Applied"])
+
+##################################################
+#
+#              Grad CAM VIZ
+#
+##################################################
+
+
+def img_is_color(img):
+
+    if len(img.shape) == 3:
+        # Check the color channels to see if they're all the same.
+        c1, c2, c3 = img[:, : , 0], img[:, :, 1], img[:, :, 2]
+        if (c1 == c2).all() and (c2 == c3).all():
+            return True
+
+    return False
+def show_image_list(list_images, list_titles=None, list_cmaps=None, grid=True, num_cols=2, figsize=(15, 7), title_fontsize=30):
+    '''
+    Shows a grid of images, where each image is a Numpy array. The images can be either
+    RGB or grayscale.
+    Parameters:
+    ----------
+    images: list
+        List of the images to be displayed.
+    list_titles: list or None
+        Optional list of titles to be shown for each image.
+    list_cmaps: list or None
+        Optional list of cmap values for each image. If None, then cmap will be
+        automatically inferred.
+    grid: boolean
+        If True, show a grid over each image
+    num_cols: int
+        Number of columns to show.
+    figsize: tuple of width, height
+        Value to be passed to pyplot.figure()
+    title_fontsize: int
+        Value to be passed to set_title().
+    '''
+
+    assert isinstance(list_images, list)
+    assert len(list_images) > 0
+    assert isinstance(list_images[0], np.ndarray)
+
+    if list_titles is not None:
+        assert isinstance(list_titles, list)
+        assert len(list_images) == len(list_titles), '%d imgs != %d titles' % (len(list_images), len(list_titles))
+
+    if list_cmaps is not None:
+        assert isinstance(list_cmaps, list)
+        assert len(list_images) == len(list_cmaps), '%d imgs != %d cmaps' % (len(list_images), len(list_cmaps))
+
+    num_images  = len(list_images)
+    num_cols    = min(num_images, num_cols)
+    num_rows    = int(num_images / num_cols) + (1 if num_images % num_cols != 0 else 0)
+
+    # Create a grid of subplots.
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    
+    # Create list of axes for easy iteration.
+    list_axes = list(axes.flat)
+
+    for i in range(num_images):
+
+        img    = list_images[i]
+        title  = list_titles[i] if list_titles is not None else 'Image %d' % (i)
+        cmap   = list_cmaps[i] if list_cmaps is not None else (None if img_is_color(img) else 'gray')
+        
+        list_axes[i].imshow(img, cmap=cmap)
+        list_axes[i].set_title(title, fontsize=title_fontsize) 
+        list_axes[i].grid(grid)
+
+    for i in range(num_images, len(list_axes)):
+        list_axes[i].set_visible(False)
+
+    fig.tight_layout()
+    _ = plt.show()
